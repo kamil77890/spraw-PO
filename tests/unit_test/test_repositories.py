f76682d43d85010fc0e9572b01387be26d2d401c
Kamil Jakubisiak <u87_kamjak_waw@technischools.com>
2024-02-22 20:34:37 +0100

    hujvc

--- tests/unit_test/test_repositories.py
+++ tests/unit_test/test_repositories.py
@@ -11,3 +11,12 @@ def test_create_user(user_repository):
     user_repository.create_user(user_data)
     assert len(user_repository.get_all_users()) == initial_user_count + 1
 
+def test_get_all_users(user_repository):
+    assert isinstance(user_repository.get_all_users(), list)
+
+def test_get_user_by_id(user_repository):
+    user_data = {"firstName": "John", "lastName": "Doe", "birthYear": "1990-01-01", "group": "user"}
+    created_user = user_repository.create_user(user_data)
+    fetched_user = user_repository.get_user_by_id(created_user["id"])
+    assert fetched_user == created_user
+
