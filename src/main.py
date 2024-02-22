06c1248ef236254c8bd8dc9cf53c9142f8534fef
Kamil Jakubisiak <u87_kamjak_waw@technischools.com>
2024-02-22 18:51:30 +0100

    4

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 from datetime import datetime
 from flask import Flask, request, Response, jsonify
 from src.controllers import UserController
-from src.users import UserRepository
+from src.users import UserService
 from src.users import User
 
 app = Flask(__name__)
-user_repository = UserRepository()
+user_repository = UserService()
 user_controller = UserController(user_repository)
 
 @app.get("/users")
@@ -18,34 +18,24 @@ def get_user_by_id(user_id):
     user = user_controller.get_by_id(user_id)
     if user:
         return jsonify(user.as_dict), 200
-    else:
-        return jsonify({"error": "User not found"}), 404
 
 @app.post("/users")
 def create_user():
     user_data = request.json
-    try:
-        user = user_controller.create(user_data)
-        return "", 201
-    except ValueError as e:
-        return jsonify({"error": str(e)}), 400
+    user = user_controller.create(user_data)
+    return "", 201
 
 @app.patch("/users/<int:user_id>")
 def update_user(user_id):
     user_data = request.json
-    try:
-        user = user_controller.update(user_id, user_data)
-        if user:
-            return "", 200
-        else:
-            return jsonify({"error": "User not found"}), 404
-    except ValueError as e:
-        return jsonify({"error": str(e)}), 400
+    user = user_controller.update(user_id, user_data)
+    if user:
+        return "", 200
+    else:
+        return jsonify({"error": "User not found"}), 404
 
 @app.delete("/users/<int:user_id>")
 def delete_user(user_id):
     success = user_controller.delete(user_id)
     if success:
         return "", 204
-    else:
-        return jsonify({"error": "User not found"}), 404
