10a40d2fb55ceede4090561be1b81e676bd6063d
Kamil Jakubisiak <u87_kamjak_waw@technischools.com>
2024-02-22 20:36:08 +0100

    bfcbfcb

--- tests/unit_test/test_repositories.py
+++ tests/unit_test/test_repositories.py
@@ -6,7 +6,7 @@ def user_repository():
     return UserRepository()
 
 def test_create_user(user_repository):
-    user_data = {"firstName": "John", "lastName": "Doe", "birthYear": "1990-01-01", "group": "user"}
+    user_data = {"firstName": "vxdvxd", "lastName": "xvdvxdv", "birthYear": "1990-01-01", "group": "user"}
     initial_user_count = len(user_repository.get_all_users())
     user_repository.create_user(user_data)
     assert len(user_repository.get_all_users()) == initial_user_count + 1
@@ -15,8 +15,21 @@ def test_get_all_users(user_repository):
     assert isinstance(user_repository.get_all_users(), list)
 
 def test_get_user_by_id(user_repository):
-    user_data = {"firstName": "John", "lastName": "Doe", "birthYear": "1990-01-01", "group": "user"}
+    user_data = {"firstName": "dvxvx", "lastName": "vdxvx", "birthYear": "1990-01-01", "group": "user"}
     created_user = user_repository.create_user(user_data)
     fetched_user = user_repository.get_user_by_id(created_user["id"])
     assert fetched_user == created_user
 
+def test_update_user(user_repository):
+    user_data = {"firstName": "vxdv", "lastName": "vxdv", "birthYear": "1990-01-01", "group": "user"}
+    created_user = user_repository.create_user(user_data)
+    updated_data = {"firstName": "vxdv", "lastName": "vxdv"}
+    updated_user = user_repository.update_user(created_user["id"], updated_data)
+    assert updated_user["firstName"] == "vxdv" and updated_user["lastName"] == "vxdv"
+
+def test_delete_user(user_repository):
+    user_data = {"firstName": "vxdv", "lastName": "vxdvxd", "birthYear": "1990-01-01", "group": "user"}
+    created_user = user_repository.create_user(user_data)
+    initial_user_count = len(user_repository.get_all_users())
+    user_repository.delete_user(created_user["id"])
+    assert len(user_repository.get_all_users()) == initial_user_count - 1
